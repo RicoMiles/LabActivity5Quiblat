f2f86b75da03fd25de8cb6f6e7bc25062cde8fba
RicoMiles <ricoquiblat@gmail.com>
2022-12-17 18:15:35 +0800

    Rename LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -7,29 +7,7 @@ public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
-    public static void main(String[] args){
-        JFrame window = new JFrame("Leap Year Checker");
-        window.setVisible(true);
-    }
-    public LeapYearGUI(String title){
-        super(title);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setContentPane(panel1);
-        this.pack();
-        btnCheckYear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                boolean flag = true;
-                int year = 0;
-                year = (int) Double.parseDouble(tfYear.getText());
-                if(year % 4 == 0){
-                    JOptionPane.showMessageDialog(null,"Leap year");
-                }else{
-                    JOptionPane.showMessageDialog(null,"Not a leap year");
-                }
-            }
-        });
-    }
+
 
 
 }
