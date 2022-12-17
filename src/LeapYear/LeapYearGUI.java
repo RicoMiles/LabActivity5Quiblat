ce78acffaaed77ed2c0e8390d7c3a7e6d549678e
RicoMiles <ricoquiblat@gmail.com>
2022-12-17 17:53:21 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,17 @@
 package LeapYear;
 
 import javax.swing.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+    public static void main(String[] args){
+        JFrame window = new JFrame("Leap Year Checker");
+        window.setVisible(true);
+    }
+
+
+
 }
