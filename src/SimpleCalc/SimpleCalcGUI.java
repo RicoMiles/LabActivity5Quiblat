c278cd9d4434976448e9f9550517a507cc897a28
RicoMiles <ricoquiblat@gmail.com>
2022-12-17 19:50:53 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -24,31 +24,36 @@ public class SimpleCalcGUI extends JFrame{
         this.setContentPane(panel1);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
+        lblResult.setEditable(false);
 
         btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double num1 = Double.parseDouble(tfNumber1.getText());
-                double num2 = Double.parseDouble(tfNumber2.getText());
-                double result = 0;
-                String op = cbOperations.getSelectedItem().toString();
-                switch(op){
-                    case "+":
-                        result = num1 + num2;
-                        lblResult.setText(String.valueOf(String.format("%.0f",result)));
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        lblResult.setText(String.valueOf(String.format("%.0f",result)));
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        lblResult.setText(String.valueOf(String.format("%.2f",result)));
-                        break;
-                    case "/":
-                        result = num1 / num2;
-                        lblResult.setText(String.valueOf(String.format("%.2f",result)));
-                        break;
+                try {
+                    double num1 = Double.parseDouble(tfNumber1.getText());
+                    double num2 = Double.parseDouble(tfNumber2.getText());
+                    double result = 0;
+                    String op = cbOperations.getSelectedItem().toString();
+                    switch (op) {
+                        case "+":
+                            result = num1 + num2;
+                            lblResult.setText(String.valueOf(String.format("%.0f", result)));
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            lblResult.setText(String.valueOf(String.format("%.0f", result)));
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            lblResult.setText(String.valueOf(String.format("%.2f", result)));
+                            break;
+                        case "/":
+                            result = num1 / num2;
+                            lblResult.setText(String.valueOf(String.format("%.2f", result)));
+                            break;
+                    }
+                } catch (Exception a) {
+                    JOptionPane.showMessageDialog(null,"Invalid Input");
                 }
             }
         });
