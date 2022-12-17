7416dc32cf8d01b3d1e4c7f990999035741255ae
RicoMiles <ricoquiblat@gmail.com>
2022-12-17 19:46:28 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,12 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfNumber1;
     private JTextField tfNumber2;
@@ -11,6 +15,43 @@ public class SimpleCalcGUI {
     private JTextField lblResult;
 
     public static void main(String[] args) {
+        JFrame window = new SimpleCalcGUI("Simple Calculator");
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+    public SimpleCalcGUI(String title){
+        super(title);
+        this.setContentPane(panel1);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(tfNumber1.getText());
+                double num2 = Double.parseDouble(tfNumber2.getText());
+                double result = 0;
+                String op = cbOperations.getSelectedItem().toString();
+                switch(op){
+                    case "+":
+                        result = num1 + num2;
+                        lblResult.setText(String.valueOf(String.format("%.0f",result)));
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        lblResult.setText(String.valueOf(String.format("%.0f",result)));
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        lblResult.setText(String.valueOf(String.format("%.2f",result)));
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        lblResult.setText(String.valueOf(String.format("%.2f",result)));
+                        break;
+                }
+            }
+        });
 
     }
 }
